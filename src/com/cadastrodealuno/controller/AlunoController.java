84b4219b9f928b283dfbe2a2577a5bf6ae2d5543
Alvaro Antonio Carneiro <carneiroalvaro320@gmail.com>
2026-09-01 16:23:20 -0300

    Update AlunoController.java

--- src/com/cadastrodealuno/controller/AlunoController.java
+++ src/com/cadastrodealuno/controller/AlunoController.java
@@ -1,1 +1,39 @@
 
+package com.cadastro-de-aluno.controller;
+
+import com.seuprojeto.model.Aluno;
+import com.seuprojeto.service.AlunoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/alunos")
+public class AlunoController {
+
+    private final AlunoService service;
+
+    public AlunoController(AlunoService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Aluno> listar() {
+        return service.listarTodos();
+    }
+
+    @PostMapping
+    public String criar(@RequestBody Aluno aluno) {
+        return service.adicionar(aluno);
+    }
+
+    @GetMapping("/{matricula}")
+    public Aluno buscar(@PathVariable String matricula) {
+        return service.buscarPorMatricula(matricula);
+    }
+
+    @DeleteMapping("/{matricula}")
+    public String deletar(@PathVariable String matricula) {
+        return service.remover(matricula);
+    }
+}
