97e87e180f73b5c6154cf53f3becc36a1ebf56b1
Alvaro Antonio Carneiro <carneiroalvaro320@gmail.com>
2026-09-01 16:21:54 -0300

    Update DemoApplication.java

--- src/com/cadastrodealuno/demo/DemoApplication.java
+++ src/com/cadastrodealuno/demo/DemoApplication.java
@@ -1,1 +1,13 @@
 
+package com.cadastrodealunos.demo;  // <--- SEM HÍFENS!
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
